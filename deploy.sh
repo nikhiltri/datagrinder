f03d6208b6cb2b8e9e690e89244673d53517608a
nikhil trivedi <ntrivedi@artic.edu>
2014-01-24 09:50:51 -0600

    Improved doco

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,6 @@
 
 sudo cp target/datagrinder.war /opt/local/share/java/tomcat6/webapps/
 echo "[deploy.sh] Copied datagrinder.war to Tomcat webapps directory.\n"
+
+scp -r * collections-sandbox:/home/ntrive/datagrinder/
+echo "[deploy.sh] Pushed code to collections-sandbox.\n"
