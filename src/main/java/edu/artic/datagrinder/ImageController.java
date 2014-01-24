cba44fe705439fa29ca712abffc910b59c20af84
nikhil trivedi <ntrivedi@artic.edu>
2014-01-24 09:28:57 -0600

    Added proper error handling

--- src/main/java/edu/artic/datagrinder/ImageController.java
+++ src/main/java/edu/artic/datagrinder/ImageController.java
@@ -2,21 +2,24 @@ package edu.artic.datagrinder;
 
 import javax.servlet.ServletException;
 
-import magick.DrawInfo;
 import magick.ImageInfo;
 import magick.MagickException;
 import magick.MagickImage;
 
-import org.springframework.web.multipart.commons.CommonsMultipartFile;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
 @Controller
 public class ImageController {
+
+  protected final Log logger = LogFactory.getLog(getClass());
 
   @RequestMapping(value="/image/resize", method = RequestMethod.GET)
   public String resizeForm() {
@@ -26,31 +29,12 @@ public class ImageController {
   @RequestMapping(value="/image/resize", method = RequestMethod.POST, produces = "image/jpeg")
   @ResponseBody
   public byte[] resizeSubmit(@RequestParam("file") CommonsMultipartFile file,
-                             @RequestParam("width") int pWidth,
-                             @RequestParam("height") int pHeight) throws ServletException {
+                             @RequestParam("width") Integer pWidth,
+                             @RequestParam("height") Integer pHeight) throws ServletException {
 
     try {
       if (file.isEmpty()) {
-        String size = "200x200";
-        if (pWidth == 0 || pHeight == 0) {
-          size = pWidth + "x" + pHeight;
-        }
-
-        ImageInfo info = new ImageInfo();
-        info.setSize(size);
-        DrawInfo drawInfo = new DrawInfo(info);
-        drawInfo.setText("No image was sent");
-        MagickImage image = new MagickImage(info);
-        image.setImageFormat("JPEG");
-        image.setCompression(85);
-        image.drawImage(drawInfo);
-
-        byte[] returnBytes = image.imageToBlob(info);
-
-        image.destroyImages();
-        image = null;
-
-        return returnBytes;
+        throw new ServletException("No image uploaded.");
       }
       else {
         byte[] tiff = file.getBytes();
@@ -60,20 +44,22 @@ public class ImageController {
         // If no dimensions were passed, use the original image's dimensions
         double origW = image.getDimension().getWidth();
         double origH = image.getDimension().getHeight();
-        if (pWidth == 0) {
-          pWidth = new Double(origW).intValue();
+        int pW = (pWidth == null ? 0 : pWidth.intValue());
+        int pH = (pHeight == null ? 0 : pHeight.intValue());
+        if (pW == 0) {
+          pW = new Double(origW).intValue();
         }
-        if (pHeight == 0) {
-          pHeight = new Double(origH).intValue();
+        if (pH == 0) {
+          pH = new Double(origH).intValue();
         }
 
         // Determine the scale ration to size the image to
         double scale = 1.0f;
-        if (pWidth > 0 && origW > pWidth) {
-          scale = (float) pWidth / origW;
+        if (pW > 0 && origW > pW) {
+          scale = (float) pW / origW;
         }
-        if (pHeight > 0 && origH > pHeight) {
-          double scaleAlt = (float) pHeight / origH;
+        if (pH > 0 && origH > pH) {
+          double scaleAlt = (float) pH / origH;
           if (scaleAlt < scale) {
             scale = scaleAlt;
           }
@@ -97,7 +83,8 @@ public class ImageController {
       }
     }
     catch (MagickException e) {
-      throw new ServletException("Problem creating an ImageMagick instance.");
+      logger.error("Problem creating an ImageMagick instance", e);
+      throw new ServletException("Problem creating an ImageMagick instance: " + e.getMessage());
     }
   }
 }
